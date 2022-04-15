fc22ee152d7b9cf236a0565eda9f03f730e2bfec
DESKTOP-SL25A5G\i <kevwang.biz@gmail.com>
2022-04-15 06:02:40 -0700

    header constructed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './Header.scss'
 
 export default () => {
     return (
-        <div>Header works!</div>
+        <div className="navigation">
+            <div className="container-navigation">
+                <div className="navigation-block-left">
+                    <a href="/" className="logo-link w-inline-block w--current">
+
+                    </a>
+                </div>
+                <div className="navigation-link-wrap">
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Services</div>
+                    </a>
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Support</div>
+                    </a>
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Contact Us</div>
+                    </a>
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Our Work</div>
+                    </a>
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Articles</div>
+                    </a>
+                    <a className="navigation-link-nu w-inline-block">
+                        <div className="navigation-text-nu">Projects</div>
+                    </a>
+                </div>
+            </div>
+        </div>
     )
 }
